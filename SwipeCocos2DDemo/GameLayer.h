9ee6a8cf1bf83a19c575f62ab4bede7bbe085006
Jon Manning <jon@secretlab.com.au>
2012-09-08 08:59:26 +1000

    Finish finish rename.

---
+++ SwipeCocos2DDemo/GameLayer.h
@@ -0,0 +1,17 @@
+//
+//  GameLayer.h
+//  LasersOfDemocracy
+//
+//  Created by Jon Manning on 30/08/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameLayer : CCLayer {
+}
+
++(CCScene *) scene;
+
+@end
---
+++ SwipeCocos2DDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LasersOfDemocracy
+//
+//  Created by Jon Manning on 30/08/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
